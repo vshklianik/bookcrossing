1176e912d404e17db5ac7d85effc7df407071a0d
MicroBelka <nikolasergel19@gmail.com>
2020-05-15 20:06:53 +0300

    Revert "Revert "Added bean validation and password encryption""
    This reverts commit eaf56f1cbcb16165183c4795cc1317928fe872c1.

--- src/main/java/by/serhel/springwebapp/controllers/RegistrationController.java
+++ src/main/java/by/serhel/springwebapp/controllers/RegistrationController.java
@@ -7,10 +7,12 @@ import by.serhel.springwebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Collections;
 import java.util.Map;
 
@@ -25,9 +27,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
+        if(user.getPassword() != null && !user.getPassword().equals(user.getPassword1())){
+            model.addAttribute("passwordError", "Password are different!");
+        }
+        model.addAttribute("user", null);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("user", user);
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+            return "registration";
+        }
+
         if(!userService.addUser(user)){
-            model.put("message", "User exist");
+            model.addAttribute("userError", "User exist");
             return "registration";
         }
         return "redirect:/login";
