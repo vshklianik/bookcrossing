d9e8386a57715a2af156088b1ffe683f60adddde
MicroBelka <nikolasergel19@gmail.com>
2020-05-15 20:51:19 +0300

    Added 'remember me', save session in database.

--- src/main/java/by/serhel/springwebapp/config/WebSecurityConfig.java
+++ src/main/java/by/serhel/springwebapp/config/WebSecurityConfig.java
@@ -2,18 +2,17 @@ package by.serhel.springwebapp.config;
 
 import by.serhel.springwebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .formLogin()
                     .loginPage("/login")
                     .permitAll()
+                .and()
+                    .rememberMe()
                 .and()
                     .logout()
                     .permitAll();
