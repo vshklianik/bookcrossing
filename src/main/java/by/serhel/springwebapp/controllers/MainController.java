70503b975e0213d6b3b6fb706baada52bd56579c
MicroBelka <nikolasergel19@gmail.com>
2020-05-19 15:55:05 +0300

    Added the ability to delete books for the admin and more info about the  book for all authorized users.

--- src/main/java/by/serhel/springwebapp/controllers/MainController.java
+++ src/main/java/by/serhel/springwebapp/controllers/MainController.java
@@ -1,16 +1,21 @@
 package by.serhel.springwebapp.controllers;
 
 import by.serhel.springwebapp.entities.Book;
+import by.serhel.springwebapp.entities.User;
 import by.serhel.springwebapp.repositories.BookRepository;
 import by.serhel.springwebapp.service.BookService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.jws.WebParam;
 
 @Controller
 public class MainController {
@@ -41,5 +46,31 @@ public class MainController {
 
         logger.info("finish 'main'");
         return "main";
+    }
+
+    @GetMapping("/main/{book}")
+    public String getUserProfile(@PathVariable Book book, Model model){
+        logger.info("start 'getUserProfile'");
+
+        if(book.getAuthor() != null){
+            User user = book.getAuthor();
+            model.addAttribute("userInfo", user);
+        }
+        model.addAttribute("book", book);
+
+        logger.info("finish 'getUserProfile'");
+        return "userProfile";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("main/delete/{book}")
+    public String deleteBook(@PathVariable Book book, Model model){
+        logger.info("start 'deleteBook'");
+
+        bookRepository.delete(book);
+        model.addAttribute("message", "success");
+
+        logger.info("finish 'deleteBook'");
+        return "redirect:/main";
     }
 }
